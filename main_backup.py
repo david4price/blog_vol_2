ea74c46482e4de51c033a8e40ac64b4c856c057f
David Praise <david.praise2323@gmail.com>
2022-10-31 16:45:02 +0200

    updated blog

--- main_backup.py
+++ main_backup.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
-from datetime import datetime, date
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import date 
+from wtforms.widgets import TextArea
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-from web_forms import PostForm, UserForm, NamerForm, PasswordForm, LoginForm, SearchForm
-from flask_ckeditor import CKEditor, CKEditorField
 
 # Create Flask Instance 
 app = Flask(__name__)
@@ -21,14 +24,11 @@ app.config['SECRET_KEY'] = "eat-my-fat_ass-bitchs"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# CKEditor
-cheditor = CKEditor(app) 
-
 
 # Flask_login
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = 'login'
 
 
 # Create Blog Post Model
@@ -36,11 +36,18 @@ class Posts(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(300))
 	content = db.Column(db.Text)
-	# author = db.Column(db.String(300))
+	author = db.Column(db.String(300))
 	date_posted = db.Column(db.DateTime, default=datetime.utcnow)
 	slug = db.Column(db.String(300))
-	# Foregin Key to Link Users
-	poster_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+
+# Create Post Form
+class PostForm(FlaskForm):
+	title = StringField("Title", validators=[DataRequired()])
+	content = StringField("Content", validators=[DataRequired()], widget=TextArea())
+	author =StringField("Author", validators=[DataRequired()])
+	slug = StringField("Slug", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 
 # Create Model
@@ -49,15 +56,10 @@ class Users(db.Model, UserMixin):
 	name = db.Column(db.String(20), nullable=False, unique=True)
 	username = db.Column(db.String(100), nullable=False)
 	email = db.Column(db.String(100), nullable=False, unique=True)
-	about_author = db.Column(db.Text(500), nullable=True)
+	favorite_music = db.Column(db.String(100))
 	date_added = db.Column(db.DateTime, default=datetime.utcnow)
-	# profile_pic = db.Column(db.String(), nullable=True)
 	# password shit 
 	password_hash = db.Column(db.String(128))
-	# User Owning Posts
-	posts = db.relationship("Posts", backref="poster")
-
-
 
 	@property
 	def password(self):
@@ -75,6 +77,35 @@ class Users(db.Model, UserMixin):
 	def __repr__(self):
 		return '<Name %r>' % self.name
 
+
+# Flask User Instance
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	username = StringField("Username", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	favorite_music = StringField("Favorite Music")
+	password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo("password_hash2", message="Passwords Must Match!")])
+	password_hash2 = PasswordField("Confirm Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+# Flask Name Instance
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
+# Flask Password Instance
+class PasswordForm(FlaskForm):
+	email = StringField("What's Your Email", validators=[DataRequired()])
+	password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
+# Login Form
+class LoginForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 # Create Custome Error Pages
 
@@ -95,18 +126,6 @@ def index():
 	stuff = "This is <strong>Bold</strong> Text"
 	return render_template("index.html",
 		first_name=first_name, stuff=stuff)
-
-
-# Admin Page
-@app.route('/admin')
-@login_required
-def admin():
-	id = current_user.id
-	if id == 15:
-		return render_template("admin.html")
-	else:
-		flash("Access Denied")
-		return redirect(url_for('dashboard'))
 
 
 @app.route('/user/<name>')
@@ -160,39 +179,6 @@ def test_pw():
 		form=form)
 
 
-
-# Pass Shit To Navbar
-@app.context_processor
-def base():
-	form = SearchForm()
-	return dict(form=form)
-
-
-# Creating User
-@app.route('/user/add', methods=['GET','POST'])
-def add_user():
-	name = None
-	form = UserForm()
-	if form.validate_on_submit():
-		# Hashing PW 
-		hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
-		user = Users.query.filter_by(email=form.email.data).first()
-		if user is None:
-			user = Users(name=form.name.data, username=form.username.data, email=form.email.data,
-			password_hash=hashed_pw)
-			db.session.add(user)
-			db.session.commit()
-		name = form.name.data
-		form.name.data = ''
-		form.username.data = ''
-		form.email.data = ''
-		form.password_hash.data = ''
-		flash("User Add Successfully")
-	our_users = Users.query.order_by(Users.date_added)
-	return render_template("add_user.html", form=form,
-		name=name, our_users=our_users)
-
-
 # Updating User Data 
 @app.route('/update/<int:id>', methods=["GET",'POST'])
 def update_user(id):
@@ -201,13 +187,12 @@ def update_user(id):
 	if request.method == "POST":
 		name_to_update.name = request.form['name']
 		name_to_update.email = request.form['email']
+		name_to_update.favorite_music = request.form['favorite_music']
 		name_to_update.username = request.form['username']
-		name_to_update.about_author = request.form['about_author']
-		name_to_update.profile_pic = request.files['profile_pic']
 		try:
 			db.session.commit()
-			flash("Profile Updated!")
-			return render_template("dashboard.html", 
+			flash("User Updated!")
+			return render_template("update.html", 
 				form=form,
 				name_to_update=name_to_update)
 		except:
@@ -242,6 +227,32 @@ def delete(id):
 			name=name, our_users=our_users)
 
 
+# Creating User
+@app.route('/user/add', methods=['GET','POST'])
+def add_user():
+	name = None
+	form = UserForm()
+	if form.validate_on_submit():
+		# Hashing PW 
+		hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			user = Users(name=form.name.data, username=form.username.data, email=form.email.data,
+			favorite_music=form.favorite_music.data, password_hash=hashed_pw)
+			db.session.add(user)
+			db.session.commit()
+		name = form.name.data
+		form.name.data = ''
+		form.username.data = ''
+		form.email.data = ''
+		form.favorite_music.data = ''
+		form.password_hash.data = ''
+		flash("User Add Successfully")
+	our_users = Users.query.order_by(Users.date_added)
+	return render_template("add_user.html", form=form,
+		name=name, our_users=our_users)
+
+
 # JSON
 @app.route('/date')
 def get_current_date():
@@ -267,13 +278,12 @@ def add_post():
 	form = PostForm()
 
 	if form.validate_on_submit():
-		poster = current_user.id
 		post = Posts(title=form.title.data, content=form.content.data,
-			poster_id=poster, slug=form.slug.data)
+			author=form.author.data, slug=form.slug.data)
 		# Clear Form
 		form.title.data = ''
 		form.content.data = ''
-		# form.author.data = ''
+		form.author.data = ''
 		form.slug.data = ''
 
 		# Add Post to DB
@@ -298,10 +308,9 @@ def posts():
 def edit_post(id):
 	post = Posts.query.get_or_404(id)
 	form = PostForm()
-
 	if form.validate_on_submit():
 		post.title = form.title.data
-		# post.author = form.author.data
+		post.author = form.author.data
 		post.slug = form.slug.data
 		post.content = form.content.data
 		# Update DB
@@ -309,46 +318,29 @@ def edit_post(id):
 		db.session.commit()
 		flash("Post Updated!")
 		return redirect(url_for('post', id=post.id))
-
-	if current_user.id == post.poster_id:
-		form.title.data = post.title
-		# form.author.data = post.author
-		form.slug.data = post.slug
-		form.content.data = post.content
-		return render_template("edit_post.html", form=form)
-
-	else:
-		flash("Insufficient Permissions")
-		# Get Posts From DB
-		posts = Posts.query.order_by(Posts.date_posted)
-		return render_template("posts.html", posts=posts)
+	form.title.data = post.title
+	form.author.data = post.author
+	form.slug.data = post.slug
+	form.content.data = post.content
+	return render_template("edit_post.html", form=form)
 
 
 # Deleting Post Page
 @app.route('/post/delete/<int:id>')
-@login_required
 def delete_post(id):
 	post_to_delete = Posts.query.get_or_404(id)
-	id = current_user.id
-	
-	if id == post_to_delete.poster.id:
-		try:
-			db.session.delete(post_to_delete)
-			db.session.commit()
-			# Message
-			flash("Deleted Successfully")
-			# Get Posts From DB
-			posts = Posts.query.order_by(Posts.date_posted)
-			return render_template("posts.html", posts=posts)
 
-		except:
-			flash("Couldn't Delete Post")
-			# Get Posts From DB
-			posts = Posts.query.order_by(Posts.date_posted)
-			return render_template("posts.html", posts=posts)
+	try:
+		db.session.delete(post_to_delete)
+		db.session.commit()
+		# Message
+		flash("Deleted Successfully")
+		# Get Posts From DB
+		posts = Posts.query.order_by(Posts.date_posted)
+		return render_template("posts.html", posts=posts)
 
-	else:
-		flash("Insufficient Permissions")
+	except:
+		flash("Couldn't Delete Post")
 		# Get Posts From DB
 		posts = Posts.query.order_by(Posts.date_posted)
 		return render_template("posts.html", posts=posts)
@@ -363,7 +355,6 @@ def load_user(user_id):
 @app.route('/login', methods=['GET','POST'])
 def login():
 	form = LoginForm()
-
 	if form.validate_on_submit():
 		user = Users.query.filter_by(username=form.username.data).first()
 		if user:
@@ -394,20 +385,4 @@ def logout():
 @login_required
 def dashboard():
 	return render_template("dashboard.html")
-
-
-# Sreach Function
-@app.route('/search', methods=['POST'])
-def search():
-	form = SearchForm()
-	posts = Posts.query
-	if form.validate_on_submit():
-		# Data From Submitted Form
-		post.searched = form.searched.data
-		# Query DB
-		posts = posts.filter(Posts.content.like('%' + post.searched + '%'))
-		posts = posts.order_by(Posts.title).all()
-
-		return render_template("search.html", form=form,
-		searched=post.searched, posts=posts)
 
